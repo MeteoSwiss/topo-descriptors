18b55eacbfa057a0a0af4bb4909337f0ab379fee
Mathieu Schaer <mathieu.schaer@meteoswiss.ch>
2020-04-27 15:05:47 +0200

    Topo descriptors TPI, Gradients, Valley and Ridge index done with documentation
    Former-commit-id: 12bae3fd4e807433327973a47d0ca18ee8c1a7f5

--- main_topo.py
+++ main_topo.py
@@ -1,36 +1,59 @@
-import numpy as np
-from env_setting import RESOURCES
-import Topo_descriptors.topo as tp
-import Topo_descriptors.sx_relief as sxrelief
-
 #%%
 # Script to compute spatial descriptors from DEM
 
+from env_setting import RESOURCES
+import Topo_descriptors.topo as tp
+import Topo_descriptors.topo_helpers as hlp
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.captureWarnings(True)
+logger = logging.getLogger(__name__)
+
+#%% get the DEM
+
+path_dem = RESOURCES / 'DEM_CCS4_50m_buffer_v-0-3.nc'
+dem_da = hlp.get_dem_netcdf(path_dem)
+inca_domain = {'chx':slice(255000,965000), 'chy':slice(-160000,480000)}
+
+#dem_da= dem_da.sel(chx=slice(300000,800000), chy=slice(00000,300000))
+ind_nans, dem_da = hlp.fill_na(dem_da)
+
 #%% Define parameters
 
-# Wich DEM to use
-dem_filename = 'dhm25_clean.nc'
-path_dem = RESOURCES /dem_filename
-
-# Sx parameters
-theta = 5
-alpha = 5
-height = 10
-d_min_sx = np.array([0,100,100,100]) # dist in meters
-d_max_sx = np.array([500,1000,2000,4000]) # dist in meters
-
-# Tpi parameters
-dist_tpi = [0.1, 0.5,2,4]  # convolution size (diameter) in km
-
-# Valley parameters
-dist_valley = [2,6,10,20] # convolution size in km
-flat_valley = [0.15,0.3] # fraction of zeros in the middle -> version with one 0 automatically included.
+topo_scales = [100, 300, 500, 1000, 2000, 4000, 6000, 10000, 20000, 30000, 
+               60000, 100000] # convolution scale in meters
 
 #%% Launch computation
        
-#if __name__ == '__main__':
-#    sxrelief.get_sxrelief_geotiff(theta, alpha, d_min_sx, d_max_sx, height, path_dem)
+# TPI
+tp.compute_tpi(dem_da, 
+               topo_scales, 
+               smth_factors=None, 
+               ind_nans=ind_nans, 
+               crop=inca_domain)
 
-tp.compute_tpi(path_dem, dist_tpi)
-tp.compute_valley(path_dem, dist_valley)
+# Gradients
+tp.compute_gradient(dem_da, 
+                    topo_scales, 
+                    sig_ratios=1, 
+                    ind_nans=ind_nans, 
+                    crop=inca_domain)
+
+# Valley Index
+tp.compute_valley_ridge(dem_da, 
+                        topo_scales,
+                        mode='valley',
+                        flat_list=[0, 0.2, 0.4], 
+                        smth_factors=None, 
+                        ind_nans=ind_nans,
+                        crop=inca_domain)
+
+# Ridge Index
+tp.compute_valley_ridge(dem_da, 
+                        topo_scales,
+                        mode='ridge',
+                        flat_list=[0, 0.1, 0.2], 
+                        smth_factors=None, 
+                        ind_nans=ind_nans,
+                        crop=inca_domain)
 
