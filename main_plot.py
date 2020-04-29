0054a1084640522adb8cd08c70d5722beaf02d64
Daniele Nerini <daniele.nerini@meteoswiss.ch>
2020-04-29 12:45:24 +0200

    Refactor main_plot.py
    Former-commit-id: 0a5f1a929b1009fd485f6bd073e6475cd6612d5d

--- main_plot.py
+++ main_plot.py
@@ -1,30 +1,59 @@
 '''Script to plot topographic descriptors maps'''
 
-from env_setting import RESOURCES
-from env_setting import DATAPATH
-import Topo_descriptors.topo_helpers as hlp
-from matplotlib import pyplot as plt
-import xarray as xr
+from env_setting import DATAPATH, RESOURCES, FIGPATH
+
+import copy
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
+import Topo_descriptors.topo_helpers as hlp
+import xarray as xr
 
-#%% get the DEM
+# optional dependency, plot national borders
+try:
+    import noborders as nob
+    NOBORDERS_IMPORTED = True
+except ModuleNotFoundError:
+    NOBORDERS_IMPORTED = False
 
-path_dem = RESOURCES / 'DEM_CCS4_50m_buffer_v-0-3.nc'
-dem_da = hlp.get_dem_netcdf(path_dem)
 inca_domain = {'chx':slice(255000,965000), 'chy':slice(-160000,480000)}
+zoom_domain = {'chx':slice(654000,704000), 'chy':slice(115500,165500)}
+zoom = patches.Rectangle(
+    (zoom_domain['chx'].start, zoom_domain['chy'].start),
+    zoom_domain['chx'].stop - zoom_domain['chx'].start,
+    zoom_domain['chy'].stop - zoom_domain['chy'].start,
+    linewidth=1.5,
+    edgecolor='k',
+    facecolor='none'
+    )
 
-dem_da= dem_da.sel(inca_domain)
+# ds = xr.open_dataset(str(DATAPATH / 'topo_TPI_60000M.nc'))
+ds = xr.open_mfdataset(str(DATAPATH / 'topo_*.nc'), combine='nested', concat_dim=None)
 
-#%% make plots
+for var in ds.data_vars:
 
-test1 = xr.open_dataset(str(DATAPATH / 'topo_TPI_60000M.nc'))
-test2 = xr.open_dataset(str(DATAPATH / 'topo_TPI_60000M_SMTHFACT0.5.nc'))
+    fig = plt.figure(figsize=(12, 4.5))
 
-area = {'chx':slice(615000,675000), 'chy':slice(130000,170000)}
+    if 'ASPECT_' in var or 'DIR_' in var:
+        robust = False
+    else:
+        robust = True
 
-plt.imshow(np.flipud(dem_da))
-plt.figure()
-plt.imshow(np.flipud(test1['TPI_60000M']))
-plt.figure()
-plt.imshow(np.flipud(test2['TPI_60000M_SMTHFACT0.5']))
+    ax1 = plt.subplot(1, 2, 1)
+    ds[var].plot.imshow(robust=robust, ax=ax1)
+    if NOBORDERS_IMPORTED: nob.addBorders(color="black", linewidth=1)
+    ax1.add_patch(copy.copy(zoom))
+    ax1.set_aspect(1)
 
+    ax2 = plt.subplot(1, 2, 2)
+    ds[var].sel(zoom_domain).plot.imshow(robust=robust, ax=ax2)
+    if NOBORDERS_IMPORTED: nob.addBorders(color="black")
+    ax2.set_xlim([zoom_domain['chx'].start, zoom_domain['chx'].stop])
+    ax2.set_ylim([zoom_domain['chy'].start, zoom_domain['chy'].stop])
+    ax2.set_aspect(1)
+
+    fig.tight_layout()
+    fn = str(FIGPATH / f'{var}.png')
+    fig.savefig(fn)
+    print(f'saved: {fn}')
+    plt.close()
